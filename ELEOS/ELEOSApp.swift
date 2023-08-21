d832704d70125da202d37860a9d6327f8171f573
toksJ <tokajaber11@gmail.com>
2023-08-21 22:18:51 +0400

    Initial Commit

---
+++ ELEOS/ELEOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  ELEOSApp.swift
+//  ELEOS
+//
+//  Created by TokaJaber on 21/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ELEOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
